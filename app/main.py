a88f2e130b18eaee3e90c91058f444e3efded69b
jim3 <jimmy3.xyz@gmail.com>
2024-07-25 15:05:56 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
-# Uncomment this to pass the first stage
 import socket
+
+BUFF_SZ = 1024
+ENC = "utf-8"
 
 
 def main():
     print("Logs from your program will appear here!")
+    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    PORT = 4221
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()  # create a listener socket
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connection from {addr} has been established...")
+            while True:
+                data = conn.recv(BUFF_SZ)
+                if not data:
+                    break
+                conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
