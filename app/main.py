4d473b6a74af4577d091a69f6e3a8bc2fad1a0df
jim3 <jimmy3.xyz@gmail.com>
2024-07-25 23:05:01 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 import socket
 
 
+# HTTP request parsing
 def parse_request(req):
     print(f"HTTP Request: ${req}")
     req_str = " ".join(req).split(" ")
     req_target = req_str[1]
+    print("req_target: ", req_target)  # debugging
 
     if req_target == "/":
         return b"HTTP/1.1 200 OK\r\n\r\n"
+    elif req_target.startswith("/echo"):
+        res_body = "".join(req_target[6:].split(" "))
+        return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(res_body)}\r\n\r\n{res_body}".encode(
+            "utf-8"
+        )
     else:
         return b"HTTP/1.1 404 Not Found\r\n\r\n"
 
@@ -21,13 +28,12 @@ def handle_connection(conn, data):
 
 
 def main():
-    print("Logs from your program will appear here!")
     HOST = "127.0.0.1"
     PORT = 4221
-
+    print("Logs from your program will appear here!")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
-        s.listen()  # create a listener socket
+        s.listen()
         conn, addr = s.accept()
         with conn:
             print(f"Connection from {addr} has been established...")
