840364208e12bb80d9ff5f9167ceb9a0ea6446ed
jim3 <jimmy3.xyz@gmail.com>
2024-07-31 15:40:16 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,17 +2,20 @@ import socket
 import argparse
 import os
 import threading
-import select
 
-import sys
 
 BUFF_SZ = 1024
 ENC = "utf-8"
+RES200 = "HTTP/1.1 200 OK\r\n"
 
 
+# `/echo/{str}` endpoint
 def echo(req):
-    req_str = " ".join(req).split(" ")  # Split the request into words
-    req_target = req_str[1]  # Extract the request target
+    # Split the request into words
+    req_str = " ".join(req).split(" ")
+    # Extract the request target path
+    req_target = req_str[1]
+    # Extract the request body from the echo endpoint `/echo/{str}`
     res_body = "".join(req_target[6:].split(" "))
     return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(res_body)}\r\n\r\n{res_body}".encode(
         "utf-8"
@@ -20,8 +23,9 @@ def echo(req):
 
 
 def user_agent(req):
-    req_str = " ".join(req).split(" ")  # Split the request into words
-    user_agent = req_str[6]  # Extract the user-agent string
+    req_str = " ".join(req).split(" ")
+    # Extract the user-agent string
+    user_agent = req_str[6]
     return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode(
         "utf-8"
     )
@@ -32,15 +36,13 @@ def get_filename(req, directory):
     req_target = req_str[1]
     # get the file name
     filename = "".join(req_target[7:].split(" "))
-
-    full_path = os.path.join(
-        directory, filename
-    )  # create the full path so you can read the fle
-
+    # create the full path so you can read the file
+    full_path = os.path.join(directory, filename)
     # Check if the file exists
     if not os.path.exists(full_path):
         return b"HTTP/1.1 404 Not Found\r\n\r\n"
 
+    # read the contents of the file and append it to the response
     with open(full_path, "rt") as file:
         data = file.read()
         print("results of data: ", data)
@@ -50,17 +52,11 @@ def get_filename(req, directory):
     )
 
 
-def post_filename(req, directory):
-    print(f"inside post_filename: HTTP Request: {req}")
+def post_req_body(req, directory):
     req_str = " ".join(req).split(" ")
     req_target = req_str[1]
-    # get the file name
     filename = "".join(req_target[7:].split(" "))
-
-    # create the full path so you can read the fle
     full_path = os.path.join(directory, filename)
-    print("full_path: ", full_path)
-
     # get the request body
     req_body = req[5]
 
@@ -68,21 +64,35 @@ def post_filename(req, directory):
     with open(full_path, "wt") as file:
         print("req_body: ", req_body)
         file.write(req_body)
-
-    # return status code
     return f"HTTP/1.1 201 Created\r\n\r\n".encode("utf-8")
+
+
+def gzip():
+    return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\n\r\n".encode(
+        "utf-8"
+    )
 
 
 # routes
 def parse_request(req, directory):
     print(f"HTTP Request: {req}")
-    req_str = " ".join(req).split(" ")  # Split the request into words
-    req_target = req_str[1]  # Extract the request target path
+    # Split the request into words
+    req_str = " ".join(req).split(" ")
+    # Extract the request target path
+    req_target = req_str[1]
+    # Used for POST
     req_method = req_str[0]
 
+    accept_encoding = req[2]
+    print("accept_encoding", accept_encoding)
+
+    if accept_encoding == "Accept-Encoding: gzip":
+        print("we got gzip!!!!!!")
+        result = gzip()
+        return result
+
     if req_method == "POST":
-        result = post_filename(req, directory)
-        print("in parse_request", result)
+        result = post_req_body(req, directory)
         return result
 
     # Handle endpoints
@@ -96,18 +106,18 @@ def parse_request(req, directory):
         return result
     elif req_target.startswith("/files"):
         result = get_filename(req, directory)
-        print("see if get_filename is called..."), result
         return result
     else:
         return b"HTTP/1.1 404 Not Found\r\n\r\n"
 
 
-def handle_connection(conn, data, directory):
+def handle_connection(conn, addr, directory):
+    print(f"Accepted new connection from {addr}")
+    # Receive the request
     data = conn.recv(BUFF_SZ)
     print(f"Raw request: {data}")
     req = data.decode(ENC).split("\r\n")
     response = parse_request(req, directory)
-
     print("Return value of response: ", response)
     conn.sendall(response)
 
@@ -116,20 +126,19 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--directory", type=str)
     args = parser.parse_args()
-
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.listen()
+    s = socket.create_server(("localhost", 4221), reuse_port=True)
+    s.listen()
     try:
         while True:
-            conn, address = server_socket.accept()
+            conn, addr = s.accept()
             client_thread = threading.Thread(
-                target=handle_connection, args=(conn, address, args.directory)
+                target=handle_connection, args=(conn, addr, args.directory)
             )
             client_thread.start()
     except KeyboardInterrupt:
         print("Server is shutting down.")
     finally:
-        server_socket.close()
+        s.close()
 
 
 if __name__ == "__main__":
